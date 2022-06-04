d906f1afb0c8439895ccaf62b825ca67b1860b9a
Misha Koshmak <peschernuy@gmail.com>
2022-06-04 17:45:10 +0300

    124 lesson add Cart total

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,27 @@
-import "./cart-dropdown.styles.scss";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import { useNavigate } from "react-router-dom";
+
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+
+import "./cart-dropdown.styles.scss";
+
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext)
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout')
+  }
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button>Chekout</Button>
+   {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
       </div>
+      <Button onClick={goToCheckoutHandler}>Checkout</Button>
     </div>
   );
 };
